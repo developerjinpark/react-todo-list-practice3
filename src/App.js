030ce90cbd2876563b5a2e3253816588dce2cae3
Jin <jinpark1504@gmail.com>
2020-04-17 13:46:31 -0400

    simple todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let id = 0;
+
+const Todo = props => (
+  <li>
+    <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle} />
+    <button onClick={props.onDelete}>Delete</button>
+    <span>{props.todo.text}</span>
+  </li>
+)
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    }
+  }
+
+  addTodo() {
+    const text = prompt("ToDo text please!!");
+    this.setState({
+      todos: [ 
+        ...this.state.todos, 
+        {id: id++, text: text, checked: false}
+      ],
+    })
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    })
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked
+        }
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div>Todo Count: {this.state.todos.length}</div>
+        <div>Unchecked Todo Count: {this.state.todos.filter(todo => !todo.checked).length}</div>
+        <button onClick={() => this.addTodo()}>Add ToDo</button>
+        <ul>
+          {this.state.todos.map(todo => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              onDelete={() => this.removeTodo(todo.id)}
+              onToggle={() => this.toggleTodo(todo.id)}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
